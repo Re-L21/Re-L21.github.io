0977b6bef0cec1013302b72ad6a9b4806a3191c3
Re-L21 <gabrielokto21@gmail.com>
2021-09-18 00:23:23 +0700

    Slide show initialize
    !!!
    - Need to fix the slider
    - Need css for the slider

--- utilities/conf/ultilities.js
+++ utilities/conf/ultilities.js
@@ -1,3 +1,26 @@
+
+var i = 0; // start point
+var images = [];
+var time = 3000;
+var imgDir = '../../images/'
+
+//img list
+images[0] = imgDir +'img1.jpg';
+images[1] = imgDir +'img2.png';
+images[2] = imgDir +'img3.png';
+
+function ChangeImg()
+{
+    document.slide.src = images[i];
+    
+    if(i < images.length -1)
+        i++;
+    else
+        i = 0;
+    
+    setTimeout("ChangeImg()",time);
+}
+
 $(function()
 {
     $("#navigation-bar").load("../../utilities/navigation-bar/navigation-bar.html");
