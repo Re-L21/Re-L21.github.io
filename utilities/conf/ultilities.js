c2f9d91083e10bff90199931462abe10eab79077
Re-L21 <gabrielokto21@gmail.com>
2021-09-17 23:20:03 +0700

    Update navbar system

---
+++ utilities/conf/ultilities.js
@@ -0,0 +1,9 @@
+$(function()
+{
+    $("#navigation-bar").load("../../utilities/navigation-bar/navigation-bar.html");
+});
+
+function Redirecting(targetPage)
+{
+    document.location.replace(targetPage);
+}
